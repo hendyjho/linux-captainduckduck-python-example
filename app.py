1bbd270698593d5af28a25d5f3c6c82b2192d455
hendyjho <72721603+hendyjho@users.noreply.github.com>
2020-10-12 17:01:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,1 @@
-from flask import Flask
-fapp = Flask(__name__)
 
-@fapp.route('/')
-def hello_world():
-    return 'Flask Dockerized 1'
-
-@fapp.route('/test')
-def hello_worlda():
-    return 'Flask Dockerized > test'
-
-
-if __name__ == '__main__':
-    #app.run(debug=True,host='0.0.0.0', port=80)
-    fapp.run(host='0.0.0.0')
